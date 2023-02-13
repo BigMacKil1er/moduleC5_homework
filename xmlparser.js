7ad2ac4f8ca1b961fd43ceff37aa14ca6cac4164
BigmacKiller <varik_varibrus@mail.ru>
2023-02-13 23:52:23 +0300

    code for parsing xml is written

---
+++ xmlparser.js
@@ -0,0 +1,41 @@
+const xmlFile = `
+<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
+
+const parser = new DOMParser();
+const xmlDOM = parser.parseFromString(xmlFile, 'text/xml');
+const bookNode = xmlDOM.querySelector('list');
+const student = bookNode.querySelectorAll('student');
+let someArray = [];
+student.forEach(element => {
+    const studentName = element.querySelector('name');
+    const firstName = studentName.querySelector('first');
+    const secondName = studentName.querySelector('second');
+    const age = element.querySelector('age')
+    const prof = element.querySelector('prof')
+    const langAtr = studentName.getAttribute('lang')
+    const person = {
+        name: firstName.textContent + ' ' + secondName.textContent,
+        age: age.textContent,
+        prof: prof.textContent,
+        lang: langAtr}
+    someArray.push(person)
+});
+
+console.log('result', someArray);
